ec649ec3a2f6f220f7a4a589f829450694dd08f0
nadirbasalamah <nadir008basalamah@gmail.com>
2021-04-27 09:14:41 +0700

    feat(api): Adding basic API function. Menambahkan function API dasar.

--- functions/2-basic-api.js
+++ functions/2-basic-api.js
@@ -1,7 +1,8 @@
-// domain/.netlify/functions/1-hello
+const items = require("../assets/data");
+
 exports.handler = async (event, context) => {
   return {
     statusCode: 200,
-    body: "Hello World!",
+    body: JSON.stringify(items),
   };
 };
